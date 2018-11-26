2048604e06063a7c81877aae64cedc586cf4a72a
robinp12 <44506208+robinp12@users.noreply.github.com>
2018-11-26 19:52:08 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,21 @@ package jeu;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Joueur joueur1 = new Joueur();
+		joueur1.initialisation();
+		joueur1.affichageTableau();
+		joueur1.ajouteRandom();
+		joueur1.affichageTableau();
+		joueur1.ajouteRandom();
+		joueur1.affichageTableau();
+		joueur1.ajouteRandom();
+		joueur1.affichageTableau();
+		joueur1.deplacement("H");
+		joueur1.affichageTableau();
+		
+		
+		
+
 
 	}
 
