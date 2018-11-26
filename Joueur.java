e84913c65286f20947c4e58fee92b7e1a701a9c1
robinp12 <44506208+robinp12@users.noreply.github.com>
2018-11-26 16:52:38 +0100

    Update Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -1,22 +1,28 @@
 package jeu;
 
+import java.util.Random;
+
 public class Joueur {
 	private int id;
 	private float timer;
 	private int score;
-	protected int[][] tableau = { 
-			{1, 2, 3, 4},
-			{5, 6, 7, 8},
-			{9, 10, 11, 12},
-			{13, 14, 15, 16}};
+	protected int [][] tableau = new int [4][4];
 	
 	
-	public void affichageTableau() {
-		int i;
-		for(i = 0; i < 4; i++){
-				System.out.println(tableau[i][0]  +"	 "+ tableau[i][1] +" 	"+ tableau[i][2] +" 	"+ tableau[i][3]) ;
-		}
-	}
+	public int random() {
+		Random r = new Random();
+        int n = r.nextInt(2);
+        if(n == 1) {
+        	n = 2;
+        }
+        else if( n == 0){
+        	n = 4;
+        }
+       return n;
+	}	
+	
+	
+	
 
 	public Joueur(int id, float timer, int score) {
 		this.id = id;
@@ -26,5 +32,28 @@ public class Joueur {
 	
 	public void deplacement() {
 		
+	}
+	
+	public void initialisation() {
+		
+		int a;
+		for(a = 0; a < 4; a++) {
+			int b;
+			for(b = 0; b < 4; b++) {
+				tableau[a][b] = 0;
+			}
+		}
+		Random r1 = new Random();
+        int i = r1.nextInt(4);
+        Random r2 = new Random();
+        int e = r2.nextInt(4);
+        tableau[i][e] = random();
+	}
+	
+	public void affichageTableau() {
+		int i;
+		for(i = 0; i < 4; i++){
+				System.out.println(tableau[i][0]  +"	 "+ tableau[i][1] +" 	"+ tableau[i][2] +" 	"+ tableau[i][3]) ;
+		}
 	}
 }
