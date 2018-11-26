16b255aee1dd55b770378c099cd7aae2c9c6908a
robinp12 <44506208+robinp12@users.noreply.github.com>
2018-11-26 15:47:42 +0100

    Update Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -4,7 +4,23 @@ public class Joueur {
 	private int id;
 	private float timer;
 	private int score;
+	protected int[][] tableau = { 
+			{1, 2, 3, 4},
+			{5, 6, 7, 8},
+			{9, 10, 11, 12},
+			{13, 14, 15, 16}};
 	
+	
+	public void affichageTableau() {
+		int i;
+		for(i = 0; i < 4; i++){
+			int e;
+			for(e = 0; e < 4; e++){
+				System.out.println(tableau[i][e]) ;
+			}
+		}
+	}
+
 	public Joueur(int id, float timer, int score) {
 		this.id = id;
 		this.timer = timer;
