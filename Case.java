5640211e37a8599c388df35215f54aaaf6d1dc2f
robinp12 <44506208+robinp12@users.noreply.github.com>
2018-11-26 16:08:16 +0100

    Update Case.java

--- Case.java
+++ Case.java
@@ -1,18 +1,29 @@
 package jeu;
 
+import java.util.Random;
+
 public class Case {
-	private int valeur;
-	
+	private int valeur = 0;
 	
 	public Case(int valeur) {
-		this.valeur = valeur;
+		
 	}
-	
+	public int random() {
+		Random r = new Random();
+        int n = r.nextInt(2);
+        if(n == 1) {
+        	n = 2;
+        }
+        else if( n == 0){
+        	n = 4;
+        }
+       return n;
+	}	
 	public void multiplication() {
 		Math.pow(valeur,2);
 	}
 	public void division() {
 		Math.pow(valeur,1/2);
 	}
-	
+
 }
