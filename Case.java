8e64c9883dd476fd6dbdddca4c8a6f252fdf6314
robinp12 <44506208+robinp12@users.noreply.github.com>
2018-11-26 16:04:23 +0100

    Update Case.java

--- Case.java
+++ Case.java
@@ -3,16 +3,16 @@ package jeu;
 public class Case {
 	private int valeur;
 	
+	
 	public Case(int valeur) {
 		this.valeur = valeur;
 	}
 	
 	public void multiplication() {
-		
+		Math.pow(valeur,2);
 	}
 	public void division() {
-		
+		Math.pow(valeur,1/2);
 	}
 	
-
 }
