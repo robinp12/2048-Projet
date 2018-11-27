0f5afb25225f773aa080ec1d3f5e06902b471cc3
robinp12 <44506208+robinp12@users.noreply.github.com>
2018-11-27 01:10:09 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,34 +6,23 @@ public class Main {
 		Joueur joueur1 = new Joueur();
 		joueur1.initialisation();
 		joueur1.affichageTableau();
+		System.out.println("Haut");
 		joueur1.deplacement("H");
 		joueur1.ajouteRandom();
 		joueur1.affichageTableau();
-		joueur1.deplacement("H");
+		System.out.println("Bas");
+		joueur1.deplacement("B");
 		joueur1.ajouteRandom();
 		joueur1.affichageTableau();
-		joueur1.deplacement("H");
-		joueur1.ajouteRandom();
-		joueur1.affichageTableau();	
-		joueur1.deplacement("H");
+		System.out.println("Gauche");
+		joueur1.deplacement("G");
 		joueur1.ajouteRandom();
 		joueur1.affichageTableau();
-		joueur1.deplacement("H");
-		joueur1.ajouteRandom();
-		joueur1.affichageTableau();
-		joueur1.deplacement("H");
-		joueur1.ajouteRandom();
-		joueur1.affichageTableau();
-		joueur1.deplacement("H");
+		System.out.println("Droite");
+		joueur1.deplacement("D");
 		joueur1.ajouteRandom();
 		joueur1.affichageTableau();
 		
-		
-		
-		
-		
-		
-
 
 	}
 
