2470411281ccf6b0280645bc1300fd4b3785bc9a
robinp12 <44506208+robinp12@users.noreply.github.com>
2018-11-27 20:55:06 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,25 +3,15 @@ package jeu;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		new Deplacement("");
+		System.out.println("appuyer sur une flèche du pavé numérique pour démarrer le 2048");
 		Joueur joueur1 = new Joueur();
 		joueur1.initialisation();
-		joueur1.affichageTableau();
-		System.out.println("Haut");
-		joueur1.deplacement("H");
-		joueur1.ajouteRandom();
-		joueur1.affichageTableau();
-		System.out.println("Bas");
-		joueur1.deplacement("B");
-		joueur1.ajouteRandom();
-		joueur1.affichageTableau();
-		System.out.println("Gauche");
-		joueur1.deplacement("G");
-		joueur1.ajouteRandom();
-		joueur1.affichageTableau();
-		System.out.println("Droite");
-		joueur1.deplacement("D");
-		joueur1.ajouteRandom();
-		joueur1.affichageTableau();
+
+		
+	
+		
 		
 
 	}
