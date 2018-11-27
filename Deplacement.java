db70ed899d19b0f560cb2948a8ec7ac178da4239
robinp12 <44506208+robinp12@users.noreply.github.com>
2018-11-27 20:54:39 +0100

    Update Deplacement.java

--- Deplacement.java
+++ Deplacement.java
@@ -6,9 +6,10 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Deplacement extends JFrame implements KeyListener {
+public class Deplacement extends JFrame  implements KeyListener {
 
     JLabel label;
+    Joueur joueur1 = new Joueur();
 
     public Deplacement(String s) {
         super(s);
@@ -18,38 +19,49 @@ public class Deplacement extends JFrame implements KeyListener {
         add(p);
         addKeyListener(this);
         setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
     }
 
-/*    @Override
+    @Override
    public void keyTyped(KeyEvent e) {
-    	 int key = e.getKeyCode();
-        if (key == KeyEvent.VK_RIGHT) {
-            System.out.println("Right key typed");
-        }
-        if (key == KeyEvent.VK_LEFT) {
-            System.out.println("Left key typed");
-        }
-
-    }*/
+    	
+    }
 
     @Override
     public void keyPressed(KeyEvent e) {
+    	
     	 int key = e.getKeyCode();
     	 if (key == KeyEvent.VK_LEFT) {
  	    	System.out.println("Gauche");
+ 	    	joueur1.deplacement("G");
+ 	    	joueur1.ajouteRandom();
+ 	    	joueur1.affichageTableau();
+ 	    	
  	    }
 
  	    if (key == KeyEvent.VK_RIGHT) {
  	    	System.out.println("Droite");
+ 	    	joueur1.deplacement("D");
+ 	    	joueur1.ajouteRandom();
+ 	    	joueur1.affichageTableau();
  	    }
 
  	    if (key == KeyEvent.VK_UP) {
  	    	System.out.println("Haut");
+ 	    	joueur1.deplacement("H");
+ 	    	joueur1.ajouteRandom();
+ 	    	joueur1.affichageTableau();
  	    }
 
  	    if (key == KeyEvent.VK_DOWN) {
  	    	System.out.println("Bas");
+ 	    	joueur1.deplacement("B");
+ 	    	joueur1.ajouteRandom();
+ 	    	joueur1.affichageTableau();
+ 	    }
+ 	    else {
+ 	    	System.setErr(null);
  	    }
 
     }
@@ -59,7 +71,5 @@ public class Deplacement extends JFrame implements KeyListener {
    
     }
 
-    public static void main(String[] args) {
-        new Deplacement("Key Listener Tester");
-    }
+
 }
