359e256864f62aaa26f87b16999ed71287f7993f
robinp12 <44506208+robinp12@users.noreply.github.com>
2018-11-27 21:40:18 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,14 +5,13 @@ public class Main {
 	public static void main(String[] args) {
 		
 		new Deplacement("");
-		System.out.println("appuyer sur une flèche du pavé numérique pour démarrer le 2048");
+		System.out.println("Bienvenue dans notre 2048 Perso ! \n"
+				+ " ©Tristan Pestiaux\n"
+				+ "  Robin Paquet\n"
+				+ "  Ludo Van Den Dorpe\n"
+				+ "Appuyer sur le pavé numérique pour démarrer !");
 		Joueur joueur1 = new Joueur();
 		joueur1.initialisation();
-
-		
-	
-		
-		
 
 	}
 
