7345ce5600d338d19b92e355992ea1360a649113
robinp12 <44506208+robinp12@users.noreply.github.com>
2018-12-05 09:31:14 +0100

    Update Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -15,7 +15,7 @@ public class Joueur extends Case {
 	}
 
 	public void initialisation() {
-
+		estBloquer = false;
 		int a;
 		for (a = 0; a < 4; a++) {
 			int b;
@@ -23,26 +23,77 @@ public class Joueur extends Case {
 				tableau[a][b] = 0;
 			}
 		}
-		Random r1 = new Random();
-		int i = r1.nextInt(4);
-		Random r2 = new Random();
-		int e = r2.nextInt(4);
-		tableau[i][e] = super.random();
+		ajouteRandom();
+		affichageTableau();
 	}
 
 	public void affichageTableau() {
 		Object affi = "";
-		int i;
-		for (i = 0; i < 4; i++) {
+		 boolean estGagner = false;
+		 boolean estPerdu = false;
+		if(!estBloquer) {
+			int i;
+			for (i = 0; i < 4; i++) {
+				for(int e = 0; e < 4; e++) {
+					if(tableau[i][e] == 64) {
+						estGagner = true;
+					}
+				}
+				
+				affi += tableau[i][0] + "\t" + tableau[i][1] + "\t" + tableau[i][2] + "\t" + tableau[i][3] + "\n";
+			}
+		}else {
+			int compteur = 0;
+			int compteur2 = 0;
+			
+			for(int i = 0; i < 4; i++) {
+				for(int e = 0; e < 4; e++) {
+					if(tableau[i][e] == 64) {
+						estGagner = true;
+						estBloquer = true;
 
-			affi += tableau[i][0] + "\t" + tableau[i][1] + "\t" + tableau[i][2] + "\t" + tableau[i][3] + "\n";
+					}
+					if(tableau[i][e] != 0) {
+							compteur++;
+						}
+					}
+				}
+			if(compteur == 16) {
+			
+				for(int i = 0; i < 3; i++) {
+					for(int e = 0; e < 4; e++) {
+						if(tableau[i][e] != tableau[i+1][e]) {
+							compteur2++;	//max 12			
+						}
+					}
+				}
+				for(int e = 0; e < 3; e++) {
+					for(int i = 0; i < 4; i++) {
+						if(tableau[i][e] != tableau[i][e+1]) {
+							compteur2++;	//max 12			
+						}
+					}
+				}
+			}
+			
+			
+			
+			if(compteur2 == 24) {
+				estPerdu = true;
+				estBloquer = true;
+
+			}
 		}
+		if(estGagner) {
+			System.out.println("Gagné !");
+		}else if(estPerdu) {
+			System.out.println("Perdu !\n appuyer sur enter pour redémarrer");
+		}else {
 		System.out.println(affi);
-
+		}
 	}
-
 	public void ajouteRandom() {
-		if (erreur) {
+		if (!estBloquer) {
 			int i, e;
 			do {
 				Random r1 = new Random();
