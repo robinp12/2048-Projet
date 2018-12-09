5e744b5ef5165846613d84c62f258ff07de66288
abyssin14 <45734363+abyssin14@users.noreply.github.com>
2018-12-09 15:51:03 +0100

    Update Deplacement.java

--- Deplacement.java
+++ Deplacement.java
@@ -14,53 +14,58 @@ public class Deplacement extends JFrame implements KeyListener{
 	public Deplacement() {
     	super();
     }
-    
-    @Override
-    public void keyTyped(KeyEvent e) {
-    	
-    }
-    @Override
-    public void keyPressed(KeyEvent e) {
-    	
-    	 int key = e.getKeyCode();
-    	 if (key == KeyEvent.VK_LEFT) {
- 	    	System.out.println("Gauche");
- 	    	joueur1.deplacement("G");
- 	    	joueur1.ajouteRandom();
- 	    	joueur1.affichageTableau();
- 	    	
- 	    }
 
- 	    if (key == KeyEvent.VK_RIGHT) {
- 	    	System.out.println("Droite");
- 	    	joueur1.deplacement("D");
- 	    	joueur1.ajouteRandom();
- 	    	joueur1.affichageTableau();
- 	    }
+	@Override
+	public void keyTyped(KeyEvent e) {
 
- 	    if (key == KeyEvent.VK_UP) {
- 	    	System.out.println("Haut");
- 	    	joueur1.deplacement("H");
- 	    	joueur1.ajouteRandom();
- 	    	joueur1.affichageTableau();
- 	    }
+	}
 
- 	    if (key == KeyEvent.VK_DOWN) {
- 	    	System.out.println("Bas");
- 	    	joueur1.deplacement("B");
- 	    	joueur1.ajouteRandom();
- 	    	joueur1.affichageTableau();
- 	    }
- 	    else {
- 	    	System.setErr(null);
- 	    }
+	@Override
+	public void keyPressed(KeyEvent e) {
 
-    }
+		int key = e.getKeyCode();
+		if (key == KeyEvent.VK_LEFT) {
+			System.out.println("Gauche");
+			joueur1.deplacement("G");
+			joueur1.ajouterAleatoire();
+			joueur1.affichageTableau();
 
-   @Override
-    public void keyReleased(KeyEvent e) {
-   
-    }
+		}
 
+		if (key == KeyEvent.VK_RIGHT) {
+			System.out.println("Droite");
+			joueur1.deplacement("D");
+			joueur1.ajouterAleatoire();
+			joueur1.affichageTableau();
+		}
+
+		if (key == KeyEvent.VK_UP) {
+			System.out.println("Haut");
+			joueur1.deplacement("H");
+			joueur1.ajouterAleatoire();
+			joueur1.affichageTableau();
+		}
+
+		if (key == KeyEvent.VK_DOWN) {
+			System.out.println("Bas");
+			joueur1.deplacement("B");
+			joueur1.ajouterAleatoire();
+			joueur1.affichageTableau();
+		}
+		if (key == KeyEvent.VK_ENTER) {
+			System.out.println("Initialisation - Reset");
+			joueur1.initialisation();
+		}
+
+		else {
+			System.setErr(null);
+		}
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+
+	}
 
 }
