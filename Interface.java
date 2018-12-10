2ab6b542ce4e0452e89ee78b5f986b53ab8abd97
robinp12 <44506208+robinp12@users.noreply.github.com>
2018-12-10 21:54:55 +0100

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -26,7 +26,7 @@ public class Interface extends Deplacement{
 			rangee1[i] = new JPanel();
 			rangee1[i].setBackground(couleurNoir);
 			rangee1[i].setSize(tailleCase, tailleCase);
-			rangee1[i].setLocation(110*i+10, 10);
+			rangee1[i].setLocation(110*i+20, 20);
 			rangee1[i].setVisible(true);
 			frame.add(rangee1[i]);
 		}
@@ -35,7 +35,7 @@ public class Interface extends Deplacement{
 			rangee2[i] = new JPanel();
 			rangee2[i].setBackground(couleurNoir);
 			rangee2[i].setSize(tailleCase, tailleCase);
-			rangee2[i].setLocation(110*i+10, 120);
+			rangee2[i].setLocation(110*i+20, 130);
 			rangee2[i].setVisible(true);
 			frame.add(rangee2[i]);
 		}
@@ -44,7 +44,7 @@ public class Interface extends Deplacement{
 			rangee3[i] = new JPanel();
 			rangee3[i].setBackground(couleurNoir);
 			rangee3[i].setSize(tailleCase, tailleCase);
-			rangee3[i].setLocation(110*i+10, 230);
+			rangee3[i].setLocation(110*i+20, 240);
 			rangee3[i].setVisible(true);
 			frame.add(rangee3[i]);
 		}
@@ -53,7 +53,7 @@ public class Interface extends Deplacement{
 			rangee4[i] = new JPanel();
 			rangee4[i].setBackground(couleurNoir);
 			rangee4[i].setSize(tailleCase, tailleCase);
-			rangee4[i].setLocation(110*i+10, 340);
+			rangee4[i].setLocation(110*i+20, 350);
 			rangee4[i].setVisible(true);
 			frame.add(rangee4[i]);
 		}
@@ -85,6 +85,55 @@ public class Interface extends Deplacement{
         }
     };
     
+    AbstractAction actionUp = new AbstractAction() {
+
+		private static final long serialVersionUID = 1L;
+
+		@Override
+        public void actionPerformed(ActionEvent e) {
+			System.out.println("Haut");
+			joueur1.deplacement("H");
+			joueur1.ajouterAleatoire();
+			joueur1.affichageTableau();
+        }
+    };    
+    AbstractAction actionDown = new AbstractAction() {
+
+		private static final long serialVersionUID = 1L;
+
+		@Override
+        public void actionPerformed(ActionEvent e) {
+			System.out.println("Bas");
+			joueur1.deplacement("B");
+			joueur1.ajouterAleatoire();
+			joueur1.affichageTableau();
+        }
+    };
+    AbstractAction actionLeft = new AbstractAction() {
+
+		private static final long serialVersionUID = 1L;
+
+		@Override
+        public void actionPerformed(ActionEvent e) {
+			System.out.println("Gauche");
+			joueur1.deplacement("G");
+			joueur1.ajouterAleatoire();
+			joueur1.affichageTableau();
+        }
+    };
+    AbstractAction actionRight = new AbstractAction() {
+
+		private static final long serialVersionUID = 1L;
+
+		@Override
+        public void actionPerformed(ActionEvent e) {
+			System.out.println("Droite");
+			joueur1.deplacement("D");
+			joueur1.ajouterAleatoire();
+			joueur1.affichageTableau();
+        }
+    };
+    
 	public Interface(int tailleCase, String s) {
 		super(s);
 		frame.setVisible(true);
@@ -95,16 +144,18 @@ public class Interface extends Deplacement{
 		
 		tuiles(tailleCase);
 		
+		JSeparator separateurHor = new JSeparator(getWidth());
+		frame.add(separateurHor);
+		
 		JLabel label = new JLabel("2048");
-        label.setSize(200, 100);
-        label.setLocation(485, 30);
-        label.setFont(new Font("Arial", Font.PLAIN, 70));
+        label.setSize(200,100);
+        label.setLocation(490, 30);
+        label.setFont(new Font("Arial", Font.BOLD, 70));
         frame.add(label);
         
-		JSeparator sep = new JSeparator(getWidth());
-		frame.add(sep);
+        JSeparator separateurVer1 = new JSeparator(getHeight());
+		frame.add(separateurVer1);
 		
-        
 		JButton demarrer = new JButton("Demarrer");
 		demarrer.setSize(150,40);
         demarrer.setLocation(490, 150);
@@ -118,6 +169,40 @@ public class Interface extends Deplacement{
         credit.setBackground(Color.LIGHT_GRAY);
         credit.addActionListener(actionCredit);
         frame.add(credit);
+        
+        JButton console = new JButton("Console");
+        console.setSize(150,40);
+        console.setLocation(490, 250);
+        console.setBackground(Color.LIGHT_GRAY);
+        console.addActionListener(actionCredit);
+        frame.add(console);
+        
+        JSeparator separateurVer2 = new JSeparator(getHeight());
+		frame.add(separateurVer2);
+        
+        JButton upBut = new JButton("U");
+        upBut.setSize(50, 40);
+        upBut.setLocation(540, 250+70);
+        upBut.addActionListener(actionUp);
+        frame.add(upBut);
+        
+        JButton downBut = new JButton("D");
+        downBut.setSize(50, 40);
+        downBut.setLocation(540, 300+70);
+        downBut.addActionListener(actionDown);
+        frame.add(downBut);
+        
+        JButton leftBut = new JButton("L");
+        leftBut.setSize(50, 40);
+        leftBut.setLocation(480, 300+70);
+        leftBut.addActionListener(actionLeft);
+        frame.add(leftBut);
+        
+        JButton rightBut = new JButton("R");
+        rightBut.setSize(50, 40);
+        rightBut.setLocation(600, 300+70);
+        rightBut.addActionListener(actionRight);
+        frame.add(rightBut);
         
 	}
 	public String texte() {
