0952ec543589b3274e7d06fa71d002e8d62d8f42
robinp12 <44506208+robinp12@users.noreply.github.com>
2018-12-18 13:36:37 +0100

    Update Main2048.java

--- Main2048.java
+++ Main2048.java
@@ -1,45 +1,41 @@
 package main;
 
+import java.awt.EventQueue;
+
 import controller.Controller2048;
 import model.Plateau;
-import view.VueGUI2048;
 import view.VueConsole2048;
-import java.lang.Runnable;
+import view.VueGUI2048;
 
-public class Main2048 extends Thread {
+public class Main2048 implements Runnable {
 
 	public synchronized void run() {
 
 		Plateau modele = new Plateau();
+		
+		Controller2048 controleurConsole = new Controller2048(modele);
+		Controller2048 controleurGui = new Controller2048(modele);
 
-		{
+		VueConsole2048 vueConsole = new VueConsole2048(modele, controleurConsole);
+		VueGUI2048 vueGui = new VueGUI2048(modele, controleurConsole);
 
-			Controller2048 controleurConsole = new Controller2048(modele);
-			Controller2048 controleurGui = new Controller2048(modele);
-
-			VueConsole2048 vueConsole = new VueConsole2048(modele, controleurConsole);
-			VueGUI2048 vueGui = new VueGUI2048(modele, controleurConsole);
-
-			controleurConsole.addView(vueConsole);
-			controleurGui.addView(vueGui);
-		}
+		controleurConsole.addView(vueConsole);
+		controleurGui.addView(vueGui);
 	}
 
 	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
 
-		Main2048 a = new Main2048();
-
-		/*
-		 * This thread will pause for 1000 milliseconds after printing each number.
-		 */
-
-		Main2048 b = new Main2048();
-
-		a.start();
-		System.out.println("Joueur1");
-		b.run();
-		System.out.println("Joueur2");
-
+            public void run() {
+            	Thread a = new Thread(new Main2048());
+				Thread b = new Thread(new Main2048());
+				/*
+				 * This thread will pause for 1000 milliseconds after printing each number.
+				 */
+				a.run();
+				b.run();
+            }
+		});
 	}
 
 }
