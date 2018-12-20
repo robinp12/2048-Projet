21f85dc1a4a735b411b752788db7f65992599589
robinp12 <44506208+robinp12@users.noreply.github.com>
2018-12-20 22:11:46 +0100

    Update Main2048.java

--- Main2048.java
+++ Main2048.java
@@ -7,33 +7,30 @@ import model.Plateau;
 import view.VueConsole2048;
 import view.VueGUI2048;
 
-public class Main2048 implements Runnable {
+public class Main2048 extends Thread {
 
-	public synchronized void run() {
-
+	public Main2048() {
 		Plateau modele = new Plateau();
 		
 		Controller2048 controleurConsole = new Controller2048(modele);
 		Controller2048 controleurGui = new Controller2048(modele);
-
+	
 		VueConsole2048 vueConsole = new VueConsole2048(modele, controleurConsole);
 		VueGUI2048 vueGui = new VueGUI2048(modele, controleurConsole);
-
+	
 		controleurConsole.addView(vueConsole);
 		controleurGui.addView(vueGui);
 	}
-
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
-
-            public void run() {
-            	Thread a = new Thread(new Main2048());
-				Thread b = new Thread(new Main2048());
-				
-				a.run();
-				b.run();
-            }
+			
+			@Override
+			public void run() {
+				Main2048 a = new Main2048();
+				Main2048 b = new Main2048();
+				a.start();
+				b.start();
+			}
 		});
 	}
-
 }
